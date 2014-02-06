e4ca44876c3aee751f4c5d26adab20ac2d080792
Karimkhan <karimkhan_it@yahoo.com>
2014-02-06 10:36:25 -0800

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', 'NwbiRH7GyiLSd5cni8ag');
-define('CONSUMER_SECRET', 'NeS730IqCg1x0RkcpPMeXMUi2bUFiqVeyxNLKiZA0dk');
-define('OAUTH_CALLBACK', 'http://www.thenwat.com/test/twitter/callback.php');
+define('CONSUMER_KEY', 'NwbiR8ag');
+define('CONSUMER_SECRET', 'NeS730IqCg1x02bUFiqVeyxNLKiZA0dk');
+define('OAUTH_CALLBACK', 'http://www.youtsite.com/test/twitter/callback.php');
 ?>
