38734e903ba818f9c53a047bb89e025ef546bf99
Karimkhan <karimkhan_it@yahoo.com>
2014-02-06 10:35:47 -0800

    Create config.php
    Add twitter app keys and secret tockens

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'NwbiRH7GyiLSd5cni8ag');
+define('CONSUMER_SECRET', 'NeS730IqCg1x0RkcpPMeXMUi2bUFiqVeyxNLKiZA0dk');
+define('OAUTH_CALLBACK', 'http://www.thenwat.com/test/twitter/callback.php');
+?>
